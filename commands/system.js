b754b217ebd804770b46a6682ba9c677b0ba53df
Alex King <139032134+Alexkingmd@users.noreply.github.com>
2023-09-10 07:51:07 +0530

    🦁RED LION POWERD BY THEEKSHNA&PRAVEEN🦁

--- commands/system.js
+++ commands/system.js
@@ -256,7 +256,7 @@ ${alivemessage}
 
 _Type ${prefix}menu for my command list._
 
-_Powered by ${Config.ownername}_
+_🦁Powered by🦁 ${Config.ownername}_
 `;
             let aliveMessage = {
                 image: {
