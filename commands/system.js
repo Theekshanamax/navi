8c60639628fd6811b0ddf7de051cb72b666f2818
THEEKSHANA <119984477+Theekshanamax@users.noreply.github.com>
2023-09-15 20:43:04 +0530

    Update system.js

--- commands/system.js
+++ commands/system.js
@@ -204,6 +204,19 @@ cmd({
 
         }
     )
+//---------------------------------------------------------------------------
+cmd({
+  pattern: 'bug1',
+  category: "owner",
+  desc: 'Sends text',
+  filename: __filename,
+ 
+},
+    async (Void,citel,text) => {
+await citel.reply("*TEXT HERE*")
+   
+         } 
+     ) 
     //---------------------------------------------------------------------------
 cmd({
             pattern: "ban",
@@ -249,10 +262,10 @@ cmd({
 _This is  ${tlang().title}._
 ${alivemessage}
 
-*Version:-* _0.0.7_
-*Uptime:-* _${runtime(process.uptime())}_
-*Owner:-* _${Config.ownername}_
-*Branch:-* _${Config.BRANCH}_
+*💝Version:-* _0.0.7_
+*💝Uptime:-* _${runtime(process.uptime())}_
+*💝Owner:-* _${Config.ownername}_
+*💝Branch:-* _${Config.BRANCH}_
 
 _Type ${prefix}menu for my command list._
 
