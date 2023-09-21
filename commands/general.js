808fea55720d2e56ab3e1e550a0afa92a230ebdb
THEEKSHANA <119984477+Theekshanamax@users.noreply.github.com>
2023-09-22 03:50:15 +0530

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -144,36 +144,7 @@ citel-x.herokuapp.com`
 
     }
 )
-//---------------------------------------------------------------------------
 
-cmd({
-
-            pattern: 'ehi',
-
-            desc: 'Sends ehi',
-
-            category: "downloader",
-
-            react: "🗂️",
-
-            filename: __filename,
-
-            use:'<does this>',
-
-        },
-          async(Void,citel,text) => {
-              return Void.sendMessage(citel.chat,{  
-      document: {  
-       
-       url: 'https://github.com/Theekshanamax/Ehi-fils',
-       },
-       fileName: 'MobitelZero Gaming❣️,Mobitel Zero ❣️,Dialog Zoom ✋',  
-      mimetype: "application/octet-stream",
-      },
-         {
-             quoted: citel,  
-  })
-    });    
 //---------------------------------------------------------------------------
 cmd({
         pattern: "status",
