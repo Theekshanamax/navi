e0eb820508c60b495a3a042bbac91321489281a2
THEEKSHANA <119984477+Theekshanamax@users.noreply.github.com>
2023-09-23 03:40:23 +0530

    Update system.js

--- commands/system.js
+++ commands/system.js
@@ -137,37 +137,6 @@ async(Void, citel, text) => {
     if ("text" in whole) { return await citel.reply('*Translated text:*\n'+whole.text); }
 }
 )
-//---------------------------------------------------------------------------
-cmd({
-            pattern: 'ehi6',
-
-	    alias :['e6','එහි6'],
-
-            desc: 'Sends ehi',
-
-            category: "ehi downloader",
-
-            react: "6️⃣",
-
-            filename: __filename,
-
-            use:'<does this>',
-
-        },
-          async(Void,citel,text) => {
-              return Void.sendMessage(citel.chat,{  
-      document: {  
-       
-       url: 'https://github.com/Theekshanamax/Ehi-fils/tree/main/ehi6',
-       },
-       fileName: 'Mobitel Zero ❣️.ehi
-',  
-      mimetype: "application/octet-stream",
-      },
-         {
-             quoted: citel,  
-  })
-    });    
     //---------------------------------------------------------------------------
 cmd({
             pattern: "shell",
