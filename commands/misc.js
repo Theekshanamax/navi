f8bc1ff5cbe281cfc86df43801f09aa0fa07c8c0
THEEKSHANA <119984477+Theekshanamax@users.noreply.github.com>
2023-09-23 03:00:01 +0530

    Update misc.js

--- commands/misc.js
+++ commands/misc.js
@@ -170,38 +170,6 @@ cmd({
              quoted: citel,  
   })
     });   
-//---------------------------------------------------------------------------
-cmd({
-            pattern: 'ehi5',
-
-	    alias :['e5','එහි5'],
-
-            desc: 'Sends ehi',
-
-            category: "ehi downloader",
-
-            react: "5️⃣",
-
-            filename: __filename,
-
-            use:'<does this>',
-
-        },
-          async(Void,citel,text) => {
-              return Void.sendMessage(citel.chat,{  
-      document: {  
-       
-       url: 'https://github.com/Theekshanamax/Ehi-fils/tree/main/ehi5',
-       },
-       fileName: 'MobitelZero Gaming❣️.ehi
-Go to file
-',  
-      mimetype: "application/octet-stream",
-      },
-         {
-             quoted: citel,  
-  })
-    });
      //---------------------------------------------------------------------------
  cmd({
              pattern: "pick",
