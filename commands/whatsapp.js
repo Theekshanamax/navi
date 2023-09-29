26d2df42d8cfdbb74ba79aa8d743127a8fbd5dff
THEEKSHANA <119984477+Theekshanamax@users.noreply.github.com>
2023-09-29 16:17:18 +0530

    Update whatsapp.js

--- commands/whatsapp.js
+++ commands/whatsapp.js
@@ -1,45 +1,4 @@
-/**
 
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                                      //
-//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
-//                                                                                                      // 
-//                                         Ｖ：１．０．１                                                // 
-//                                                                                                      // 
-//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
-//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
-//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
-//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
-//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
-//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
-//                                                                                                      //
-//                                                                                                      //
-//                                                                                                      //
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-
-CURRENTLY RUNNING ON BETA VERSION!!
-*
-   * @project_name : Suhail-Md
-   * @author : Suhail Tech Info
-   * @youtube : https://www.youtube.com/c/@SuhailTechInfo0
-   * @description : Suhail-Md ,A Multi-functional whatsapp user bot.
-   * @version 1.2.1
-*
-   * Licensed under the  GPL-3.0 License;
-* 
-   * Created By Suhail Tech Info.
-   * © 2023 Suhail-Md.
-* 
-   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-   * SOFTWARE.
-
- ⛥
- **/
 
  const moment = require("moment-timezone")
  const Config = require('../config')
@@ -52,37 +11,37 @@ const {updateProfilePicture,forwardMessage, getAdmin} = require("../lib")
 
 
 ///============================================================================================
-smd({ pattern: "ptv", desc: "send ptv Message of video", category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+cmd({ pattern: "ptv", desc: "send ptv Message of video", category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
    if(!msg.quoted) return await msg.send("*Uhh please, reply to a video Message*")
    let mtype = msg.quoted.mtype 
    if(mtype !== "videoMessage") return await msg.send("*Replied Message is not a video, Idiot.*")
    return await forwardMessage(msg.chat, Suhail, msg, cmdName )
 })////------------------------------------------------------------
-smd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+cmd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
    if(!msg.quoted) return await msg.send("*Uhh Please, reply to whatsapp status*")
    let sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
    return await forwardMessage(sᴜʜᴀɪʟ_ᴍᴅ_num, Suhail, msg, cmdName )
 })//--------------------------------------------------------------------
-smd({pattern: "save",desc: "Save Message to log number",category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+cmd({pattern: "save",desc: "Save Message to log number",category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
    if(!isCreator) return await msg.send(tlang().owner)
    if(!msg.quoted) return await msg.send("*Uhh Please, reply to a Message*")
    let sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
    return await forwardMessage(sᴜʜᴀɪʟ_ᴍᴅ_num, Suhail, msg, cmdName )
 })///================================================================================
-smd({ on: "text" }, async (Suhail,msg,text)=> {
+cmd({ on: "text" }, async (Suhail,msg,text)=> {
    if(msg.quoted && msg.text.toLowerCase().includes("send") ){
        let sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
        if(msg.quoted.sender === sᴜʜᴀɪʟ_ᴍᴅ_num && msg.quoted.chat === 'status@broadcast' ){ return await forwardMessage(msg.chat, Suhail.bot, msg, 'send' ); }
    }
 })//==================================================================
 
-smd({pattern: "pp",desc: "Set profile picture",category: "whatsapp",use: '<reply to image>', filename: __filename,},
+cmd({pattern: "pp",desc: "Set profile picture",category: "whatsapp",use: '<reply to image>', filename: __filename,},
 async(Suhail, msg,text,{cmdName , isCreator}) => {
    if(!isCreator) return await msg.send(tlang().owner);if (!msg.quoted) return await msg.reply("*Reply to an image, dear*");
    if(msg.quoted.mtype !='imageMessage') return await msg.reply("*_Uhh please, Reply to an image._*");    
    const sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user); return await updateProfilePicture(Suhail , sᴜʜᴀɪʟ_ᴍᴅ_num , msg , 'pp' );
  })
-smd({ pattern: "fullpp", desc: "Set full screen profile picture", category: "whatsapp", use: '<reply to image>', filename: __filename,},
+cmd({ pattern: "fullpp", desc: "Set full screen profile picture", category: "whatsapp", use: '<reply to image>', filename: __filename,},
  async(Suhail, msg,text,{cmdName , isCreator}) => {
    if(!isCreator) return await msg.send(tlang().owner);if (!msg.quoted) return await msg.reply("*Reply to an image, sir*");
    if(msg.quoted.mtype !='imageMessage') return await msg.reply("*_Uhh please, Reply to an image._*");
